3175b59bbb7b3d5cdd603239ce955745909ef74d
wenzhenglin <wenzhenglin@qianbao.com>
2018-09-28 18:04:55 +0800

    add vendor

---
+++ vendor/github.com/chinglinwen/checkup/go.mod
@@ -0,0 +1,3 @@
+module github.com/chinglinwen/checkup
+
+require github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
--- vendor/github.com/chinglinwen/checkup/qbnotice.go
+++ vendor/github.com/chinglinwen/checkup/qbnotice.go
@@ -2,8 +2,11 @@
 package checkup
 
 import (
-	"log"
+	"fmt"
 	"wen/svc-d/notice"
+
+	"github.com/chinglinwen/checkup/cache"
+	"github.com/chinglinwen/log"
 )
 
 var (
@@ -13,7 +16,20 @@ var (
 
 // it's depend on svc-d for flag setting
 func Send(content, status, expire string) {
-	reply, err := notice.Send(DefaultReceiver, content, status, expire)
+	// avoid repeat sent
+	user := DefaultReceiver
+	if d, found := cache.Get(user, content, status); found {
+		e := fmt.Sprintf("user: %v, content: %v not expired in %v, skip send\n",
+			user, content, d.Format("15:04:05"))
+		log.Debug.Printf(e)
+		return
+	}
+
+	// set cache
+	log.Printf("user %v, %v, status: %v, expire set to %v\n", user, content, status, expire)
+	cache.Set(user, content, status, expire)
+
+	reply, err := notice.Send(user, content, status, expire)
 	if err != nil {
 		log.Printf("send alertall err: %v, resp: %v\n", err, reply)
 	} else {
